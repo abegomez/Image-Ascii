dead74636aa17d8ae3cd610918b7ff497611a3be
Abraham Gomez <agomez717@gmail.com>
2019-08-06 16:37:32 -0700

    Convert image to 2d Color array

--- Image Ascii/Program.cs
+++ Image Ascii/Program.cs
@@ -14,75 +14,54 @@ namespace Image_Ascii
     {
         static void Main(string[] args)
         {
-            string file = "C:\\Users\\ryuhyoko\\Source\\Repos\\Image Ascii\\Image Ascii\\black-square.jpg";
+            string file = "C:\\Users\\ryuhyoko\\Source\\Repos\\Image Ascii\\Image Ascii\\ow.jpg";
             int imageWidth = 0;
             int imageHeight = 0;
             Image image = Image.FromFile(file);
-            byte[] photoBytes = File.ReadAllBytes(file);
             
-            // Format is automatically detected though can be changed.
-            ISupportedImageFormat format = new JpegFormat { Quality = 70 };
-            Size size = new Size(150, 0);
-            //using (MemoryStream inStream = new MemoryStream(photoBytes))
-            //{
-            //    using (MemoryStream outStream = new MemoryStream())
-            //    {
-            //        // Initialize the ImageFactory using the overload to preserve EXIF metadata.
-            //        using (ImageFactory imageFactory = new ImageFactory(preserveExifData: false))
-            //        {
-            //            // Load, resize, set the format and quality and save an image.
-            //            imageFactory.Load(inStream)
-            //                        .Format(format)
-            //                        .Save(outStream);
-            //            imageWidth = imageFactory.Image.Width;
-            //            imageHeight = imageFactory.Image.Height;
-            //            Console.WriteLine("Image width:" + imageFactory.Image.Width);
-            //            Console.WriteLine("Image height:" + imageFactory.Image.Height);
-
-            //        }
-            //        // Do something with the stream.
-            //        //Console.WriteLine( "byte length:{0}", outStream.Length);
-
-            //        //pixelMatrix = ConvertArray(image, imageWidth);
-            //        string val;
-            //        Console.Write("Enter integer: ");
-            //        val = Console.ReadLine();
-            //    }
-            //}
             Console.WriteLine("image width: {0}", image.Width);
             Console.WriteLine("image height: {0}", image.Height);
-            ImageConverter imgCon = new ImageConverter();
-            byte[] photoByte = (byte[])imgCon.ConvertTo(image, typeof(byte[]));
 
-            byte[,] pixelMatrix = To2dArray(photoByte, image.Width);
+            var ms = new MemoryStream();
+            image.Save(ms, System.Drawing.Imaging.ImageFormat.Bmp);
+            byte[] photoByte = ms.ToArray();
+
+            Color[,] pixelMatrix = To2dArray(photoByte, image.Width);
+            Console.WriteLine("height: {0}", photoByte.Length / image.Width/3);
+            
             string val;
             Console.Write("Enter integer: ");
             val = Console.ReadLine();
         }
-        static byte[,] To2dArray(byte[] source, int width)
+
+        static int GetAverageRGB(Color c)
         {
-            int height = source.Length / width;
-            byte[,] result = new byte[height,width];
+            int avg = (c.R + c.G + c.B) / 3;
+            return avg;
+        }
+
+
+        //TODO
+        static int[,] ConvertToBrightnessMatrix(Color[,] mat)
+        {
+            int[,] = new int[mat.
+        }
+        static Color[,] To2dArray(byte[] source, int width)
+        {
+            int height = source.Length / width/3;
+            Color[,] result = new Color[height,width];
             for (int i = 0; i < height; i++)
             {
-                for (int j = 0; j < width; j++)
+                for (int j = 0; j < width; j+=3)
                 {
-                    result[i,j] = source[i * width + j];
+                    int r = source[i * width + j];
+                    int g = source[i * width + j+1];
+                    int b = source[i * width + j+2];
+                    Color c = Color.FromArgb(r, g, b);
+                    result[i,j] = c;
                 }
             }
             return result;
-        }
-        public static byte[,] ConvertArray(byte[] Input, int size)
-        {
-            byte[,] Output = new byte[(int)(Input.Length / size), size];
-            for (int i = 0; i < Input.Length; i += size)
-            {
-                for (int j = 0; j < size; j++)
-                {
-                    //Output[(int)(i / size), j] = Input[i + j];
-                }
-            }
-            return Output;
         }
     }
 }
